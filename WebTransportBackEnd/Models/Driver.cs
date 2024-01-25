b7eeaa628e827df5bc82426e65b1fd1205189980
Jaime <jaimerr2008@hotmail.com>
2024-01-25 09:51:00 -0500

    Creacion de modelo y controlador de Drivers, configuracion de autorizacion por jwt

---
+++ WebTransportBackEnd/Models/Driver.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebTransportBackEnd.Models
+{
+    public class Driver
+    {
+        [Column(TypeName = "INT")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public string last_name { get; set; }
+        public string first_name { get; set; }
+        public string ssn { get; set; }
+        public DateTime dob { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string zip { get; set; }
+        public long phone { get; set; }
+        public bool active { get; set; }
+    }
+}
